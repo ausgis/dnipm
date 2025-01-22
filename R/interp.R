bd46b2375c66f1037d6d6b81dea9f02ba54ac886
SpatLyu <lyu.geosocial@gmail.com>
2025-01-22 22:33:15 +0800

    update `interp`

--- R/interp.R
+++ R/interp.R
@@ -4,6 +4,7 @@
 #' @param coords The coordinate matrix
 #' @param predicts Matrix of locations to predict
 #' @param method (optional) Interpolation method
+#' @param polyorder (optional) Degree of the polynomial
 #' @param na.rm (optional) Whether `NA` values should be stripped
 #'
 #' @return A numeric vector
@@ -14,7 +15,8 @@
 #' interp(ndvi$NDVI,ndvi[,c("X","Y")],
 #'        matrix(c(2745,2455),ncol = 2))
 #'
-interp = \(z, coords, predicts, method = "bilinear", na.rm = TRUE){
+interp = \(z, coords, predicts, method = "bilinear",
+           polyorder = 3, na.rm = TRUE){
   predicts = as.matrix(predicts)
   coords = as.matrix(coords)
   xyzs = .xyz_vec2mat(z,coords)
@@ -26,6 +28,9 @@ interp = \(z, coords, predicts, method = "bilinear", na.rm = TRUE){
     res = bicubicInterp(predicts,xyzs[[2]],xyzs[[3]],xyzs[[1]],na.rm)
   } else if (method == "bezier") {
     res = bezierInterp(predicts,xyzs[[2]],xyzs[[3]],xyzs[[1]],na.rm)
+  }else if (method == "bspline") {
+    res = bsplineInterp(predicts,xyzs[[2]],xyzs[[3]],xyzs[[1]],
+                        polyorder, polyorder, na.rm)
   }
   return(res)
 }
